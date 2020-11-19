970a9d42752279cb42ed193946beb8d43016feb8
MennaHisham <mennahishamh@gmail.com>
2020-11-19 11:43:43 +0100

    mongo connected to atlas and id routing done

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 const routes = require("./routes");
 app.use(routes);
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log(`Listening on ${process.env.PORT}`);
 });
