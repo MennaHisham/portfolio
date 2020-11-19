1df8ac5a97190b4cc08ce3119e52320f5aae89af
MennaHisham <mennahishamh@gmail.com>
2020-11-19 13:14:57 +0100

    installed morgan

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 require("./db");
 require("dotenv").config();
 const express = require("express");
+const logger = require("morgan");
 const app = express();
 const PORT= process.env.PORT || 8000;
 
-const routes = require("./routes");
-app.use(routes);
+app.use(express.json());
+
+
+app.use(logger("dev"));
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
 }
+const routes = require("./routes");
+app.use(routes);
+
+app.use((req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 app.listen(PORT , function () {
   console.log(`Listening on ${PORT}`);
